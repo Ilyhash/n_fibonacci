c5c27a4998503e8575211c06ffc101208044dd44
Ilyahash <astfgl96@gmail.com>
2020-03-24 13:57:49 +0200

    Better naming

--- n_fibonacci.py
+++ n_fibonacci.py
@@ -1,23 +1,23 @@
 def n_fibonacci():
-    num_n = int(input("The N-parameter is: "))
-    num_m = int(input("The M-parameter is: "))
+    base = int(input("On how many previous numbers is the next one based? "))
+    length = int(input("How many numbers do you want to print? "))
     to_print = []
-    if num_n == 0 or num_n == 1:
-        for i in range(num_m):
+    if base == 0 or base == 1:
+        for i in range(length):
             to_print.append(0)
     else:
-        for i in range(num_n - 1):
+        for i in range(base - 1):
             to_print.append(0)
         to_print.append(1)
         num = 0
-        for o in range(num_m - num_n):
-            for i in range(num_n):
+        for o in range(length - base):
+            for i in range(base):
                 index = len(to_print) - 1 - i
                 nums = to_print[index]
                 num += nums
             to_print += [num]
             num = 0
-    sequence = to_print[:num_m]
+    sequence = to_print[:length]
     return sequence
 
 
